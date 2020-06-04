a4c42840b7d2ef880760a3b67b66f6bb6c3c47e6
songxuguang <kemplaw@hotmail.com>
2020-06-04 13:48:45 +0800

    @update: 初始化多页面 webpack 配置

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,7 +1,10 @@
+import React from 'react'
 import { connect } from 'react-redux'
 import './App.css'
 
-function App(props) {}
+function App(props) {
+  return <div>app</div>
+}
 
 export default connect(
   function mapStateToProps(state) {},
